72227863ca4251127ddde1cb770b6250161f40fe
Naam me kya rakha hai <guptapriyanshu973@gmail.com>
2023-05-13 00:36:02 +0530

    some more changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::net::IpAddr;
 use std::str::FromStr;
+use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 
 struct Arguments {
     flag: String,
@@ -24,13 +27,36 @@ impl Arguments {
                 println!("Usage: -j to select how many threads you want
                 \r\n    -h or --help to show this help message");
                 return Err("help");
+            } else if flag.contains("-h") || flag.contains("--help") {
+                return Err("too many arguments");
+            } else if flag.contains("-j") {
+                let ipaddr = match IpAddr::from_str(&args[3]) {
+                    Ok(s) => s,
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
+                };
+                let threads = match args[2].parse::<u16>() {
+                    Ok(s) => s,
+                    Err(_) => return Err("failed to parse threads numbers")
+                };
+                return Ok(Arguments{threads, flag, ipaddr});
+            } else {
+                return Err("invalid syntax");
             }
         }
     }
-
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err| {
+            if err.contains("help") {
+                process::exit(0);
+            } else {
+                eprintln!("{} problems parsing arguments: {}", program, err);
+                process::exit(0);
+            }
+        }
+    );
 }
